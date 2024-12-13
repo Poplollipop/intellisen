b98c7afc5d0f154ff1a14c57a992fafd317bf7cb
Li <aaa111334@gmail.com>
2024-12-14 00:13:29 +0800

    modified toolbar add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "./layouts/toolbar/toolbar.component";
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ToolbarComponent,
+    MatDialogModule,
+    MatButtonModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
