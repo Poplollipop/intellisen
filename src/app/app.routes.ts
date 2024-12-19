9b9d4b3d0bda439e5f275276167144b884cc4523
Jerry <aa29003748@gmail.com>
2024-12-19 21:42:17 +0800

    little update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { ResultPageComponent } from './page/result-page/result-page.component';
 import { HomePageComponent } from './page/home-page/home-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo:'/home', pathMatch:'full' },       // 導向首頁
   { path: 'home', component: HomePageComponent },           // 主畫面路由
   { path: 'result', component: ResultPageComponent },       // 結果頁面路由
-  { path: '', component: HomePageComponent }
 ];
