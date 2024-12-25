5ed6306e9fd4ba69d8a78a475702c47a7af5df09
Rikka Chiu <shigoto312141@gmail.com>
2024-12-25 17:46:01 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ResultPageComponent } from './page/result-page/result-page.component';
 import { HomePageComponent } from './page/home-page/home-page.component';
 import { SearchPageComponent } from './page/search-page/search-page.component';
+import { SearchResultPageComponent } from './page/search-result-page/search-result-page.component';
 
 
 export const routes: Routes = [
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'home', component: HomePageComponent },           // 主畫面路由
   { path: 'result', component: ResultPageComponent },       // 結果頁面路由
   { path: 'search', component: SearchPageComponent },       // 搜尋頁路由
+  { path: 'search-result', component: SearchResultPageComponent } // 搜尋結果頁面
 ];
--- src/app/page/search-result-page/search-result-page.component.ts
+++ src/app/page/search-result-page/search-result-page.component.ts
@@ -1,11 +1,72 @@
+import { serverRoutes } from './../../app.routes.server';
 import { Component } from '@angular/core';
+import { count } from 'console';
+import { ScrollPanel } from 'primeng/scrollpanel';
 
 @Component({
   selector: 'app-search-result-page',
-  imports: [],
+  imports: [ ScrollPanel ],
   templateUrl: './search-result-page.component.html',
   styleUrl: './search-result-page.component.scss'
 })
 export class SearchResultPageComponent {
+  court!: string; // 裁判法院
+  id!: string; // 裁判字號
+  charge!: string; // 案由
+  caseType!: string; // 案件類型
+  docType!: string; // 種類，如裁定或判決等
+  judgeName!: string; //法官姓名
+  defendantName!: string; // 被告姓名
+  date!: string; // 裁判日期
+
+  searchResultData!: any; // 假資料
+
+  ngOnInit(): void {
+    this.searchResultData = {
+      '113,訴,557': {
+        id: '113,訴,557',
+        court: '新北地方方法院',
+        charge: '詐欺',
+        caseType: '刑事案件',
+        docType: '判決',
+        judgeName: '王大明',
+        defendantName: '張三',
+        date: '2024-12-25'
+      },
+      '114,訴,558': {
+        id: '114,訴,558',
+        court: '台北地方方法院',
+        charge: '竊盜',
+        caseType: '刑事案件',
+        docType: '判決',
+        judgeName: '李小華',
+        defendantName: '李四',
+        date: '2024-12-20'
+      },
+      '115,訴,559': {
+        id: '114,訴,558',
+        court: '高雄地方方法院',
+        charge: '傷害',
+        caseType: '刑事案件',
+        docType: '判決',
+        judgeName: '林大榮',
+        defendantName: '王五',
+        date: '2024-12-18'
+      }
+    };
+
+    this.id = this.searchResultData.id;
+    this.court = this.searchResultData.court;
+    this.charge = this.searchResultData.charge;
+    this.caseType = this.searchResultData.caseType;
+    this.docType = this.searchResultData.docType;
+    this.judgeName = this.searchResultData.judgeName;
+    this.defendantName = this.searchResultData.defendantName;
+    this.date = this.searchResultData.date;
+
+  }
+
+
 
 }
+
