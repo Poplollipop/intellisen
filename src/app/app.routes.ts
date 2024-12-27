51828f7b70ede2bd74d7de052fe60cd4e0dbf052
zy <aabbcc4120@gmail.com>
2024-12-27 09:39:52 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { SearchPageComponent } from './page/search-page/search-page.component';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo:'/home', pathMatch:'full' },       // 導向首頁
+  { path: '', redirectTo:'/search', pathMatch:'full' },     // 導向首頁
   { path: 'home', component: HomePageComponent },           // 主畫面路由
   { path: 'result', component: ResultPageComponent },       // 結果頁面路由
   { path: 'search', component: SearchPageComponent },       // 搜尋頁路由
