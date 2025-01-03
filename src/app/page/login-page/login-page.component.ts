7f50a74c2fa16ceb2d9e83f25fad69e37f231007
Rikka Chiu <shigoto312141@gmail.com>
2025-01-03 17:21:32 +0800

    Integrate registration and login APIs.

--- src/app/page/login-page/login-page.component.ts
+++ src/app/page/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { PasswordModule } from 'primeng/password';
+import { HttpClientService } from '../../service/http-client.service';
 
 
 
@@ -17,24 +18,24 @@ import { PasswordModule } from 'primeng/password';
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
   account!: string;
   password!: string;
-  errorMessage: string = '';    // 錯誤提示訊息
-
+  errorMessage: string = ''; // 錯誤提示訊息
+  loginData!: login;
 
   constructor(
     private router: Router,
-  ){}
+    private http: HttpClientService
+  ){ }
 
   // 驗證email格式
-  accountValidation(input: string):boolean{
+  accountValidation(input: string): boolean {
     const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;;
     return regex.test(input);
   }
 
   // 顯示email錯誤
-  updateEmailForm(){
+  updateEmailForm() {
     const isValid = this.accountValidation(this.account);
 
     if (isValid) {
@@ -42,12 +43,32 @@ export class LoginPageComponent {
     } else {
       this.errorMessage = '請輸入正確Email格式!';
     }
-
   }
 
-
-  gotoregister(){
+  gotoregister() {
     this.router.navigateByUrl('/register')
   }
 
+  login() {
+    this.loginData = {
+      email: this.account,
+      password: this.password
+    }
+
+    console.log(this.loginData);
+
+    this.http.postApi('http://localhost:8080/accountSystem/login', this.loginData)
+    .subscribe((res: any) => {
+      console.log(res);
+      if (res.code == 400) {
+        return alert('test');
+      }
+    });
+  }
+
+}
+
+interface login {
+  email: string;
+  password: string
 }
--- src/app/page/register-page/register-page.component.ts
+++ src/app/page/register-page/register-page.component.ts
@@ -1,22 +1,63 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { HttpClientService } from '../../service/http-client.service';
 
 @Component({
   selector: 'app-register-page',
   imports: [
     MatButtonModule,
+    FormsModule
   ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  email!: string;
+  password!: string;
+  registerData!: register; // 註冊資料
+
 
   constructor(
     private router: Router,
-  ) {}
+    private http: HttpClientService,
+  ) { }
 
-  goback(){
+  // 註冊
+  register() {
+    // 檢查 email 格式
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(this.email)) {
+      return alert('請確認 Email 格式');
+    }
+
+    // 密碼不得為空或空白字串
+    if (!this.password || !this.password.trim()) {
+      return alert('請確認密碼格式');
+    }
+
+
+    this.registerData = {
+      email: this.email,
+      password: this.password
+    }
+
+    this.http.postApi('http://localhost:8080/accountSystem/register', this.registerData)
+      .subscribe((res: any) => {
+        console.log(res);
+        if (res.code == 400) {
+          return alert('Email 已重複，請確認填寫');
+        }
+      });
+  }
+
+  goback() {
     this.router.navigateByUrl('/login')
   }
+}
+
+interface register {
+  email: string;
+  password: string
 }
