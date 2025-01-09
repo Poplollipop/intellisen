ac8c8ab6fc8fefee8a9693b78387ef3fdd5a9624
Jerry <aa29003748@gmail.com>
2025-01-09 14:01:26 +0800

    update

--- src/app/page/search-result-page/search-result-page.component.ts
+++ src/app/page/search-result-page/search-result-page.component.ts
@@ -306,8 +306,8 @@ export class SearchResultPageComponent {
   }
 
   // 觀看全文
-  checkConternt(id : String){
+  checkConternt(id : string){
     // 將網址與案件 id 綁在一起
-    this.router.navigateByUrl('full-text/:' +  id);
+    this.router.navigateByUrl('full-text/:' + id);
   }
 }
