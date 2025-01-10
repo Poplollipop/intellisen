1abe965d3c0d8c68cd77a052979af24b72427f07
Jerry <aa29003748@gmail.com>
2025-01-10 14:23:19 +0800

    debug

--- src/app/page/search-result-page/search-result-page.component.ts
+++ src/app/page/search-result-page/search-result-page.component.ts
@@ -263,6 +263,7 @@ export class SearchResultPageComponent {
   }
 
   goCombinedId(): string{
+    this.combinedId = '';
     // 將三個輸入框的值合併成一個新值
     if(this.year){
       this.combinedId = this.combinedId + `${this.year}年度`
@@ -297,6 +298,8 @@ export class SearchResultPageComponent {
     }
 
     const savedConditions = tidyData;
+    console.log(savedConditions);
+
     this.searchApi(savedConditions);
   }
 
@@ -313,7 +316,7 @@ export class SearchResultPageComponent {
     this.updateVisibleCases(); // 更新顯示的筆數
   }
 
-  // 
+  //
   updateVisibleCases(): void {
     const start = this.first;   // 更新起始的那一筆的 index
     const end = this.first + this.itemsPerPage;   // 更新結束的那一筆的 index
