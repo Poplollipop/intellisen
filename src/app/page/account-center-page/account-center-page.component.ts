711fe648a457b42703065e00c9cb0238da863639
yong <aaa>
2025-01-16 00:48:44 +0800

    大概把更新資料的部分寫好/更新登入註冊畫面/明天可能會遲到一小下

--- src/app/page/account-center-page/account-center-page.component.ts
+++ src/app/page/account-center-page/account-center-page.component.ts
@@ -59,48 +59,5 @@ export class AccountCenterPageComponent {
     this.router.navigateByUrl('/edit-info')
   }
 
-  // 刪除帳戶
-  confirmDelete() {
-    this.deleteUserData = {
-      email: this.email
-    }
-
-    this.http.postApi2('http://localhost:8080/accountSystem/delete-user', this.deleteUserData).subscribe({
-      next: (response: any) => {
-        if (response.body.code == 200) {
-          Swal.fire({
-            title: '刪除帳戶通知',
-            text: '確定要刪除嗎？',
-            icon: 'info',
-            showCancelButton: true,   // 顯示取消按鈕
-            confirmButtonText: '確定',
-            cancelButtonText: '取消'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                text: '您已成功刪除。',
-                icon: 'info',
-                confirmButtonText: '關閉'
-              });
-              this.session.clearIsLogin();
-              this.router.navigateByUrl('/search')
-            }
-          });
-        } else {
-          Swal.fire({
-            text: '刪除帳戶失敗',
-            icon: 'error',
-            confirmButtonText: '確定'
-          });
-        }
-      },
-      error: (error) => {
-        Swal.fire({
-          text: '刪除帳戶失敗',
-          icon: 'error',
-          confirmButtonText: '確定'
-        });
-      }
-    });
-  }
+  
 }
--- src/app/page/account-center-page/delete-account/delete-account.component.ts
+++ src/app/page/account-center-page/delete-account/delete-account.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClientService } from '../../../service/http-client.service';
+import { SessionServiceService } from '../../../service/session-service.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-delete-account',
@@ -8,4 +12,57 @@ import { Component } from '@angular/core';
 })
 export class DeleteAccountComponent {
 
+  deleteUserData!: any;
+  email!: string;
+
+  constructor(
+    private http: HttpClientService,
+    private session: SessionServiceService,
+    private router: Router
+  ) {}
+  
+  // 刪除帳戶
+  confirmDelete() {
+    this.deleteUserData = {
+      email: this.session.getEmail()
+    }
+
+    this.http.postApi2('http://localhost:8080/accountSystem/delete-user', this.deleteUserData).subscribe({
+      next: (response: any) => {
+        if (response.body.code == 200) {
+          Swal.fire({
+            title: '刪除帳戶通知',
+            text: '確定要刪除嗎？',
+            icon: 'info',
+            showCancelButton: true,   // 顯示取消按鈕
+            confirmButtonText: '確定',
+            cancelButtonText: '取消'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                text: '您已成功刪除。',
+                icon: 'info',
+                confirmButtonText: '關閉'
+              });
+              this.session.clearIsLogin();
+              this.router.navigateByUrl('/search')
+            }
+          });
+        } else {
+          Swal.fire({
+            text: '刪除帳戶失敗',
+            icon: 'error',
+            confirmButtonText: '確定'
+          });
+        }
+      },
+      error: (error) => {
+        Swal.fire({
+          text: '刪除帳戶失敗',
+          icon: 'error',
+          confirmButtonText: '確定'
+        });
+      }
+    });
+  }
 }
