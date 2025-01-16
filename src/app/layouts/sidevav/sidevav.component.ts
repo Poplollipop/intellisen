71a29c07927b0b86c530aaa7e305954aa73ddc00
yong <aaa>
2025-01-16 12:12:47 +0800

    更2

--- src/app/layouts/sidevav/sidevav.component.ts
+++ src/app/layouts/sidevav/sidevav.component.ts
@@ -3,11 +3,12 @@ import { SessionServiceService } from '../../service/session-service.service';
 import { HttpClientService } from '../../service/http-client.service';
 import { isPlatformBrowser } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-sidevav',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, MatIconModule],
   templateUrl: './sidevav.component.html',
   styleUrl: './sidevav.component.scss',
 })
@@ -16,7 +17,7 @@ export class SidevavComponent {
 
   role: string | null = '';
   name: string | null = '';
-  menuItems: { label: string; link: string }[] = [];
+  menuItems: { icon: string ; label: string; link: string }[] = [];
 
   constructor(
     public session: SessionServiceService,
@@ -35,38 +36,40 @@ export class SidevavComponent {
         this.role = '事務所'
         this.menuItems = [
           // { label: '首頁', link: '/account-center/account-main' },
-          { label: '個人資料', link: '/account-center/account-profile' },
-          { label: '書籤', link: '/account-center/my-bookmarks' },
-          { label: '觀看紀錄', link: '/account-center/historical-record' },
-          { label: '成員管理', link: '/account-center/members-management' },
-          // { label: '刪除帳號', link: '/account-center/delete-account' },
+          { icon: "person" , label: '個人資料', link: '/account-center/account-profile' },
+          { icon: 'bookmark' , label: '書籤', link: '/account-center/my-bookmarks' },
+          { icon: 'history' , label: '觀看紀錄', link: '/account-center/historical-record' },
+          { icon: 'manage_accounts' , label: '成員管理', link: '/account-center/members-management' },
+          { icon: 'person_remove' , label: '刪除帳號', link: '/account-center/delete-account' },
         ];
         break;
       case 'lawyer':
         this.role = '律師'
         this.menuItems = [
           // { label: '首頁', link: '/account-center/account-main' },
-          { label: '個人資料', link: '/account-center/account-profile' },
-          { label: '書籤', link: '/account-center/my-bookmarks' },
-          { label: '觀看紀錄', link: '/account-center/historical-record' },
-          { label: '客戶管理', link: '/account-center/client-management' },
-          // { label: '刪除帳號', link: '/account-center/delete-account' },
+          { icon: "person" ,  label: '個人資料', link: '/account-center/account-profile' },
+          { icon: 'bookmark' ,  label: '書籤', link: '/account-center/my-bookmarks' },
+          { icon: 'history' ,  label: '觀看紀錄', link: '/account-center/historical-record' },
+          // { icon: 'manage_accounts' ,  label: '客戶管理', link: '/account-center/client-management' },
+          { icon: 'person_remove' ,  label: '刪除帳號', link: '/account-center/delete-account' },
         ];
         break;
       case 'user':
         this.role = '一般使用者'
         this.menuItems = [
           // { label: '首頁', link: '/account-center/account-main' },
-          { label: '個人資料', link: '/account-center/account-profile' },
-          { label: '書籤', link: '/account-center/my-bookmarks' },
-          { label: '觀看紀錄', link: '/account-center/' },
+          { icon: 'person' ,  label: '個人資料', link: '/account-center/account-profile' },
+          { icon: 'bookmark' ,  label: '書籤', link: '/account-center/my-bookmarks' },
+          { icon: 'history' ,  label: '觀看紀錄', link: '/account-center/' },
+          { icon: 'person_remove' ,  label: '刪除帳號', link: '/account-center/delete-account' },
         ];
         break;
       case 'guest':
         this.role = '訪客'
         this.menuItems = [
           // { label: '首頁', link: '/account-center/account-main' },
-          { label: '個人資料', link: '/account-center/account-profile' },
+          { icon: 'person' ,  label: '個人資料', link: '/account-center/account-profile' },
+          { icon: 'person_remove' ,  label: '刪除帳號', link: '/account-center/delete-account' },
         ];
         break;
     }
