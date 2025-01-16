49b3ee11f599790cf660155e5bdd45a0a3d3b6dc
Rikka Chiu <shigoto312141@gmail.com>
2025-01-16 21:08:02 +0800

    修正刪除帳號功能

--- src/app/page/account-center-page/delete-account/delete-account.component.ts
+++ src/app/page/account-center-page/delete-account/delete-account.component.ts
@@ -19,48 +19,48 @@ export class DeleteAccountComponent {
     private http: HttpClientService,
     private session: SessionServiceService,
     private router: Router
-  ) {}
-  
+  ) { }
+
   // 刪除帳戶
   confirmDelete() {
     this.deleteUserData = {
       email: this.session.getEmail()
     }
 
-    this.http.postApi2('http://localhost:8080/accountSystem/delete-user', this.deleteUserData).subscribe({
-      next: (response: any) => {
-        if (response.body.code == 200) {
-          Swal.fire({
-            title: '刪除帳戶通知',
-            text: '確定要刪除嗎？',
-            icon: 'info',
-            showCancelButton: true,   // 顯示取消按鈕
-            confirmButtonText: '確定',
-            cancelButtonText: '取消'
-          }).then((result) => {
-            if (result.isConfirmed) {
+    Swal.fire({
+      title: '刪除帳戶通知',
+      text: '確定要刪除嗎？',
+      icon: 'info',
+      showCancelButton: true,   // 顯示取消按鈕
+      confirmButtonText: '確定',
+      cancelButtonText: '取消'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.postApi2('http://localhost:8080/accountSystem/delete-user', this.deleteUserData).subscribe({
+          next: (response: any) => {
+            if (response.body.code == 200) {
               Swal.fire({
                 text: '您已成功刪除。',
                 icon: 'info',
                 confirmButtonText: '關閉'
               });
               this.session.clearIsLogin();
-              this.router.navigateByUrl('/search')
+              this.router.navigateByUrl('/search');
+            } else {
+              Swal.fire({
+                text: '刪除帳戶失敗',
+                icon: 'error',
+                confirmButtonText: '確定'
+              });
             }
-          });
-        } else {
-          Swal.fire({
-            text: '刪除帳戶失敗',
-            icon: 'error',
-            confirmButtonText: '確定'
-          });
-        }
-      },
-      error: (error) => {
-        Swal.fire({
-          text: '刪除帳戶失敗',
-          icon: 'error',
-          confirmButtonText: '確定'
+          },
+          error: (error) => {
+            Swal.fire({
+              text: '刪除帳戶失敗',
+              icon: 'error',
+              confirmButtonText: '確定'
+            });
+          }
         });
       }
     });
